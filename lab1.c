8cd250e75369c0078f1f96e72ce78bc96cbebbb8
Yurii <yura20xa@gmail.com>
2022-11-28 13:30:52 +0200

    adding my lab with eauation

---
+++ lab1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float x, y, z;
+        x = 4.123;
+        y = 2.395;
+    
+    /*printf("Type x = " );
+    scanf("%f", &x);
+    printf("Type y = " );  
+    
+    scanf("%f", &y);*/
+    
+    z = log(pow(x,3)) + tan(y) + pow((y+x),3) - (x*sin((y+1.6))/(177*pow(x,2)));
+
+    printf("%f", z);
+    
+    }
